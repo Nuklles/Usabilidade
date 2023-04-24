b011e707e96ce38bffcfbc0e8d1440011668c6c6
Nuklles <103897836+Nuklles@users.noreply.github.com>
2023-04-24 11:11:20 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<center>
+	<form id="form_login" action="autenticar.php" method="POST">
+		Login: <input type="text" name="login" required><br>
+		Senha: <input type="password" name="senha" required><br>
+		<input type="submit" name="enviar" value="Entrar"> 
+	</form>
+	</center>
+</body>
+</html>
